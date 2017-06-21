d4a57468453bf2602ee71cf06482199abaa6e5ed
Mathias <mathias.descoins@epitech.eu>
2017-06-21 09:36:12 +0200

    ch

---
+++ requirement.c
@@ -0,0 +1,24 @@
+/*
+** requirement.c for  in /home/mathias/Bureau/projet_epitech/synthèse/ADM_projTester_2016
+** 
+** Made by Mathias
+** Login   <mathias.descoin@epitech.eu@epitech.net>
+** 
+** Started on  Wed Jun 21 09:05:29 2017 Mathias
+** Last update Wed Jun 21 09:36:03 2017 Mathias
+*/
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+void my_ps_synthesis()
+{
+  int pid;
+
+  if ((pid = fork() == 0))
+    execl("/bin/sh", "sh", "-c", "ps", (char *) 0);
+  else if (pid > 0)
+    wait(&pid);
+}
