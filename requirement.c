80befb35bcd096b97e7d5730d5029c1ab5711c9f
Mathias <mathias.descoins@epitech.eu>
2017-06-22 09:17:19 +0200

    ch

--- requirement.c
+++ requirement.c
@@ -5,7 +5,7 @@
 ** Login   <mathias.descoin@epitech.eu@epitech.net>
 ** 
 ** Started on  Wed Jun 21 09:05:29 2017 Mathias
-** Last update Wed Jun 21 09:36:03 2017 Mathias
+** Last update Thu Jun 22 09:17:08 2017 Mathias
 */
 
 #include <unistd.h>
@@ -17,7 +17,7 @@ void my_ps_synthesis()
 {
   int pid;
 
-  if ((pid = fork() == 0))
+  if ((pid = fork()) == 0)
     execl("/bin/sh", "sh", "-c", "ps", (char *) 0);
   else if (pid > 0)
     wait(&pid);
